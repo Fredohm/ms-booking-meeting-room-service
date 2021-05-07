c2381cda3c4df2b7c87d2adcf521acce3ecdac9c
Fredohm <fredwaucquez@hotmail.fr>
2021-05-07 10:24:00 +0200

    Initial commit

---
+++ src/main/java/com/fredohm603/msbookingmeetingroomservice/MsBookingMeetingRoomServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fredohm603.msbookingmeetingroomservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBookingMeetingRoomServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsBookingMeetingRoomServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fredohm603/msbookingmeetingroomservice/MsBookingMeetingRoomServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fredohm603.msbookingmeetingroomservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBookingMeetingRoomServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
